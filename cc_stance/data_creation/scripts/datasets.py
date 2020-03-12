6158570ee6aa3eb476cc8f7e18f9ed3bc8c229b4
Yiwei Luo <yiweiluo@DN51sc3q.SUNet>
2020-03-11 17:18:18 -0700

    add and update data creation scripts

--- cc_stance/data_creation/scripts/datasets.py
+++ cc_stance/data_creation/scripts/datasets.py
@@ -7,25 +7,27 @@ import os
 
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
-
+from statistics import *
 
 def _stance(path, train_data_type=None, test_data_type=None):
     def clean_ascii(text):
         # function to remove non-ASCII chars from data
         return ''.join(i for i in str(text) if ord(i) < 128)
-    orig = pd.read_csv(path, delimiter=None, header=0, encoding = "latin-1", index_col = 0)
+    orig = pd.read_pickle(path)#, encoding = "latin-1", index_col = 0)
     orig['text'] = orig['text'].apply(clean_ascii)
     df = orig
     #print(df.shape)
+
     # Get only those items from a single data type in the training data
+    if test_data_type is not None:
+        test_df = df.loc[df.type == test_data_type]
+    #print(test_df.shape)
+
     if train_data_type is not None:
         train_df = df.loc[(df.type == train_data_type) & (df.type != test_data_type)]
     else:
         train_df = df.loc[df.type != test_data_type]
-    #print(train_df.shape)
-
-    if test_data_type is not None:
-        test_df = df.loc[df.type == test_data_type]
+        #print(train_df.shape)
 
     stances = ["agree", "neutral", "disagree"]
     class_nums = {s: i for i, s in enumerate(stances)}
@@ -38,12 +40,103 @@ def _stance(path, train_data_type=None, test_data_type=None):
 
     train_Y_by_stance = {s: np.array([class_nums[x] for x in train_df_by_stance[s].stance]) for i,s in enumerate(stances)}
     test_Y_by_stance = {s: np.array([class_nums[x] for x in test_df_by_stance[s].stance]) for i,s in enumerate(stances)}
-    #print(len(train_X),len(train_Y),len(test_X),len(test_Y))
+
     return train_X_by_stance, train_Y_by_stance, test_X_by_stance, test_Y_by_stance
+
+def flat_stance(data_dir):
+    path = Path(data_dir)
+    datafile = 'flat_mturk_df.pkl'
+    flat_mturk_df = pd.read_pickle(path/datafile)
+    flat_mturk_source_groups = flat_mturk_df.groupby('source')
+    train_sources, dev_test_sources = train_test_split(flat_mturk_source_groups.first().index,
+             test_size=0.3, random_state=seed1)
+    dev_sources,test_sources = train_test_split(dev_test_sources,test_size=0.6,random_state=seed2)
+    train_df = flat_mturk_df[flat_mturk_df.source.isin(train_sources)]
+    dev_df = flat_mturk_df[flat_mturk_df.source.isin(dev_sources)]
+    test_df = flat_mturk_df[flat_mturk_df.source.isin(test_sources)]
+
+    train_X = train_df.text.values
+    train_Y = train_df.stance.values
+
+    dev_source_groups = dev_df.groupby('source')
+    dev_X,dev_Y = [],[]
+    for source in dev_source_groups.first().index:
+        group = dev_source_groups.get_group(source)
+        dev_X.append(group.text.values[0])
+        try:
+            dev_Y.append(mode(group.stance.values))
+        except StatisticsError:
+            dev_Y.append(group.MACE_pred.values[0])
+
+    test_source_groups = test_df.groupby('source')
+    test_X,test_Y = [],[]
+    for source in test_source_groups.first().index:
+        group = test_source_groups.get_group(source)
+        test_X.append(group.text.values[0])
+        try:
+            test_Y.append(mode(group.stance.values))
+        except StatisticsError:
+            test_Y.append(group.MACE_pred.values[0])
+
+    train_df = pd.DataFrame({'stance':train_Y,'text':train_X})
+    dev_df = pd.DataFrame({'stance':dev_Y,'text':dev_X})
+    test_df = pd.DataFrame({'stance':test_Y,'text':test_X})
+    #print(train_df.head())
+    p#rint(train_df.stance.value_counts())
+    #print(test_df.head())
+    stances = ["agrees", "neutral", "disagrees"]
+    class_nums = {s: i for i, s in enumerate(stances)}
+    train_df_by_stance = {s: train_df.loc[train_df.stance == s] for s in stances}
+    dev_df_by_stance = {s: dev_df.loc[dev_df.stance == s] for s in stances}
+    test_df_by_stance = {s: test_df.loc[test_df.stance == s] for s in stances}
+    #print(train_df_by_stance)
+
+    train_X_by_stance = {s: train_df_by_stance[s].text.values for i,s in enumerate(stances)}
+    dev_X_by_stance = {s: dev_df_by_stance[s].text.values for i,s in enumerate(stances)}
+    test_X_by_stance = {s: test_df_by_stance[s].text.values for i,s in enumerate(stances)}
+
+    train_Y_by_stance = {s: np.array([class_nums[x] for x in train_df_by_stance[s].stance]) for i,s in enumerate(stances)}
+    dev_Y_by_stance = {s: np.array([class_nums[x] for x in dev_df_by_stance[s].stance]) for i,s in enumerate(stances)}
+    test_Y_by_stance = {s: np.array([class_nums[x] for x in test_df_by_stance[s].stance]) for i,s in enumerate(stances)}
+    #print(train_Y_by_stance)
+
+    if args.downsample:
+        min_N = min([len(train_X_by_stance[s]) for s in stances])
+        print('Downsampling to ~{} examples per stance.'.format(min_N))
+        for s in stances:
+            train_X_by_stance[s] = train_X_by_stance[s][:min_N+200]
+
+    trX = []
+    trY = []
+    for i,s in enumerate(stances):
+        for t, y in zip(train_X_by_stance[s], train_Y_by_stance[s]):
+            trX.append(t)
+            trY.append(y)
+
+    teX = []
+    teY = []
+    for i,s in enumerate(stances):
+        for t, y in zip(test_X_by_stance[s], test_Y_by_stance[s]):
+            teX.append(t)
+            teY.append(y)
+
+    vaX = []
+    vaY = []
+    for i,s in enumerate(stances):
+        for t, y in zip(dev_X_by_stance[s], dev_Y_by_stance[s]):
+            vaX.append(t)
+            vaY.append(y)
+
+    trY = np.asarray(trY, dtype=np.int32)
+    vaY = np.asarray(vaY, dtype=np.int32)
+    teY = np.asarray(teY, dtype=np.int32)
+    #pd.DataFrame({'sentence':teX,'stance':teY,'type':[test_data_type]*len(teX)}).to_csv(data_dir+'/test_data_{}.csv'.format(test_data_type))
+
+    return (trX, trY), (vaX, vaY), (teX, teY)
 
 def stance(data_dir, train_data_type=None, test_data_type=None, splits=None):
     path = Path(data_dir)
-    datafile = 'labeled_data_df'
+    datafile = 'labeled_data_df.pkl'
 
     train_X_by_stance, train_Y_by_stance, test_X_by_stance, test_Y_by_stance = _stance(path/datafile, train_data_type=train_data_type, test_data_type=test_data_type)
     stances = ["agree", "neutral", "disagree"]
@@ -56,7 +149,11 @@ def stance(data_dir, train_data_type=None, test_data_type=None, splits=None):
         test_Y = test_Y_by_stance[s]
         assert len(test_X) == len(test_Y)
         proportion = test_props_by_stance[s]
-        retrain_X_by_stance[s],teX_by_stance[s],retrain_Y_by_stance[s],teY_by_stance[s] = train_test_split(test_X_by_stance[s], test_Y_by_stance[s], test_size=proportion, random_state=seed1)
+        if len(test_Y) > 0:
+            retrain_X_by_stance[s],teX_by_stance[s],retrain_Y_by_stance[s],teY_by_stance[s] = train_test_split(test_X_by_stance[s], test_Y_by_stance[s], test_size=proportion, random_state=seed1)
+        else: # if there are 0 examples of a given stance in the test set
+            teX_by_stance[s],teY_by_stance[s] = [],[]
+            retrain_X_by_stance[s],retrain_Y_by_stance[s] = [],[]
     #print(len(retrain_X),len(retrain_Y),len(teX),len(teY))
     for i,s in enumerate(stances):
         train_X_by_stance[s] = np.concatenate([train_X_by_stance[s],retrain_X_by_stance[s]],axis=0)
@@ -114,6 +211,7 @@ if __name__ == "__main__":
     parser.add_argument('--neutral_split_size', type=float, default=0.5)
     parser.add_argument('--agree_split_size', type=float, default=0.5)
     parser.add_argument('--downsample',action='store_true')
+    parser.add_argument('--flatten',action='store_true') # includes each MTurk label by an annotator as a datapoint
 
     args = parser.parse_args()
     print(args)
@@ -133,7 +231,10 @@ if __name__ == "__main__":
     seed2 = 1236454357
     #data_dir = "../data"
 
-    (trX, trY), (vaX, vaY), (teX, teY) = stance(data_dir,train_data_type=train_data_type,test_data_type=test_data_type,splits=split_sizes)
+    if args.flatten:
+        (trX, trY), (vaX, vaY), (teX, teY) = flat_stance(data_dir)
+    else:
+        (trX, trY), (vaX, vaY), (teX, teY) = stance(data_dir,train_data_type=train_data_type,test_data_type=test_data_type,splits=split_sizes)
 
     print(trX[:5], trY[:5])
     print(len(trX))
