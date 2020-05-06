9c93ee996056ab9b9f8a9a663bd22ae25848ba45
Yiwei Luo <yiweiluo@Yiweis-new-Air.fios-router.home>
2020-05-06 11:40:47 -0400

    update notebooks

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 import re
 import glob
+import json
 
 from urllib.error import URLError
 import urllib
@@ -25,21 +26,28 @@ def soupify(url):
 # fulltext_dir_2='/Users/yiweiluo/scientific-debates/1_data_scraping/cc_fulltexts/'
 # fnames = set(os.listdir(fulltext_dir)) | set(os.listdir(fulltext_dir_2))
 
-fulltext_dir = 'url_texts'
+fulltext_dir = './1_data_scraping/url_texts'
+meta_dir = './1_data_scraping/url_meta'
 fnames = set(os.listdir(fulltext_dir))
+metanames = set(os.listdir(meta_dir))
 
 def fulltext_exists(url_guid):
     return '{}.txt'.format(url_guid) in fnames
 
-def get_guid(url):
-    return df.loc[df.url==url]['guid']
+def meta_exists(url_guid):
+    return '{}.json'.format(url_guid) in metanames
 
-def get_fulltext(url):
-    guid = get_guid(url)
+def get_fulltext(url_guid):
     if fulltext_exists(url_guid):
-        with open(os.path.join(fulltext_dir,guid+'.txt'),'r') as f:
+        with open(os.path.join(fulltext_dir,url_guid+'.txt'),'r') as f:
             lines = f.readlines()
         if len(lines) > 0:
             return lines[0]
         return ""
     return ""
+
+def get_meta(url_guid):
+    if meta_exists(url_guid):
+        with open(os.path.join(meta_dir,url_guid+'.json'),'r') as f:
+            json_obj = json.loads(f.read())
+        return json_obj
