652f653247c65ca51acf9fd6979933fdf4f257be
Yiwei Luo <yiweiluo@Yiweis-new-Air.fios-router.home>
2020-06-25 16:21:08 -0400

    reformat mediacloud_ids to .txt, add instructions to readme, update 0_get_urls.py

--- 1_data_scraping/0_get_urls.py
+++ 1_data_scraping/0_get_urls.py
@@ -1,12 +1,6 @@
-# Import statements
+#!/usr/bin/env python
+
 import argparse
-import urllib
-import requests
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-from dateutil.parser import parse
-from dateutil import parser
-from collections import defaultdict
 import re
 import pickle
 import os
@@ -15,28 +9,12 @@ import numpy as np
 import glob
 import datetime
 import json
+from dateutil import parser
+from collections import defaultdict
 
 config = json.load(open('../config.json', 'r'))
 MC_API_KEY = config['MC_API_KEY']
 SERP_API_KEY = config['SERP_API_KEY']
-
-# Set up MediaCloud API
-import mediacloud.api
-with open('MC_API_KEY.txt','r') as f:
-    MC_API_KEY = f.read()
-mc = mediacloud.api.MediaCloud(MC_API_KEY)
-mc_metadata = ['ap_syndicated','language','media_id','media_name','publish_date','title','guid','url','word_count']
-mc_ids = pd.read_pickle('mediacloud_ids.pkl')
-mc_ids.reset_index(drop=True, inplace=True)
-
-# Set up SerpAPI
-from serpapi.google_search_results import GoogleSearchResults
-if SERP_API_KEY == "":
-    with open('SERP_API_KEY.txt','r') as f:
-        SERP_API_KEY = f.read()
-query_params = {"location":"United States", "device":"desktop", "hl":"en", "gl":"us", "serp_api_key":SERP_API_KEY}
-CC_KEYWORDS = ['climate_change','global_warming','fossil_fuels','carbon_dioxide','co2']
-client = GoogleSearchResults(query_params)
 
 def do_serpapi(domain,keyword):
     """
@@ -59,6 +37,7 @@ def do_serpapi(domain,keyword):
 
     return dict_list
 
+
 def parse_serpapi_results(d_list):
     """
     Script for parsing results returned from do_serpapi().
@@ -80,23 +59,29 @@ def parse_serpapi_results(d_list):
     return meta
 
 
-def get_mc_urls():
+def get_mc_urls(start_year=2000,start_mo=1,start_day=1,end_year=2020,end_mo=12,end_day=31):
     """
     Generates `mediacloud_df.pkl`, a dataframe w/ output from MediaCloud.
+    :param int start_year: Year to begin collecting stories.
+    :param int start_mo: Month to begin collecting stories.
+    :param int start_day: Day to begin collecting stories.
+    :param int end_year: Year to end story collection.
+    :param int end_mo: Month to end story collection.
+    :param int end_day: Day to end story collection.
     """
-    # Collect stories from each outlet using MediaCloud
     if not os.path.exists('./mediacloud'):
         os.mkdir('./mediacloud')
+
     for curr_outlet_ix in mc_ids.index:
         curr_outlet_id = mc_ids.iloc[curr_outlet_ix]['media_id']
         curr_outlet_stance = mc_ids.iloc[curr_outlet_ix]['leaning']
         fetch_size = 5000
         stories = []
         last_processed_stories_id = 0
-        for start_year in range(2020,2021,5): # Start collecting stories from Jan. 1, 2000
+        for start_year in range(start_year,end_year,5): # Start collecting stories from Jan. 1, 2000 by default
             while len(stories) < 10000:
                 fetched_stories = mc.storyList('(climate AND chang*) OR (global AND warming) OR (carbon AND dioxide) OR (co2) OR (fossil AND fuel*) AND media_id:{}'.format(curr_outlet_id),
-                                               solr_filter=mc.publish_date_query(datetime.date(start_year,1,1), datetime.date(start_year+4,12,31)),
+                                               solr_filter=mc.publish_date_query(datetime.date(start_year,start_mo,start_day), datetime.date(start_year+4,end_mo,end_day)),
                                                last_processed_stories_id=last_processed_stories_id, rows= fetch_size)
                 stories.extend(fetched_stories)
                 if len( fetched_stories) < fetch_size:
@@ -122,29 +107,17 @@ def get_mc_urls():
     df_all['clean_title'] = df_all.title.apply(lambda x: re.sub(r'[^a-zA-Z0-9\s]', '', x.lower()))
     df_all.to_pickle('mediacloud_df.pkl')
 
-def get_serp_urls(domain_list=None):
+
+def get_serp_urls(l_domains,r_domains):
     """
     Generates `google_search_res_climate_change_n.pkl`, a dictionary with outer keys for domains and inner keys for search terms.
-    :param domain_list: list of domains. If None, will default to domains in `google_domains.txt`.
     """
-
-    # Read in list of domains and political leanings for SerpAPI
-    if domain_list is None:
-        L_DOMAINS,R_DOMAINS = [],[]
-        with open('google_domains.txt','r') as f:
-            lines = f.readlines()
-            for line in lines:
-                split_line = line.split()
-                R_DOMAINS.append(split_line[0]) if split_line[1] == 'R' else L_DOMAINS.append(split_line[0])
-    else:
-        L_DOMAINS = domain_list['L']
-        R_DOMAINS = domain_list['R']
 
     # Initialize default nested dict with outer keys for each media domain and inner keys for each keyword.
     URLS_PER_DOMAIN = defaultdict(dict)
 
     # Query each domain for each keyword
-    for DOMAIN in L_DOMAINS + R_DOMAINS:
+    for DOMAIN in l_domains + r_domains:
         for KW in CC_KEYWORDS:
             dl = do_serpapi(DOMAIN,KW)
             results = parse_serpapi_results(dl)
@@ -159,37 +132,8 @@ def get_serp_urls(domain_list=None):
     pickle.dump(URLS_PER_DOMAIN,open(save_name,'wb'))
     print('Done!')
 
-def get_google_res_stance(x):
-    """
-    Returns pro- if L-wing, anti- if R-wing.
-    :param x: a str URL.
-    :return: str indicating outlet stance toward CC.
-    """
-    if 'foxnews.com' in x:
-        return 'anti'
-    elif 'breitbart.com' in x:
-        return 'anti'
-    elif 'blaze.com' in x:
-        return 'anti'
-    elif 'pjmedia.com' in x:
-        return 'anti'
-    elif 'nationalreview.com' in x:
-        return 'anti'
-    elif 'dailycaller.com' in x:
-        return 'anti'
-    elif 'reason.com' in x:
-        return 'anti'
-    elif 'americanthinker.com' in x:
-        return 'anti'
-    elif 'redstate.com' in x:
-        return 'anti'
-    elif 'infowars.com' in x:
-        return 'anti'
-    else:
-        return 'pro'
 
-
-def create_filtered_df(r_domains,l_domains):
+def create_filtered_df(l_domains=None,r_domains=None):
     """Creates intermediate df for collected URLs, applying filtering, regularization, and deduplication."""
 
     BLACKLIST_URL_INIT_STRS = set(['rss.','feeds.','rssfeeds.'])
@@ -211,7 +155,7 @@ def create_filtered_df(r_domains,l_domains):
         return False
 
     def is_pdf(url):
-        return url[-4:] == '.pdf':
+        return url[-4:] == '.pdf'
 
     from urllib.parse import urlparse
 
@@ -227,7 +171,6 @@ def create_filtered_df(r_domains,l_domains):
         """Strip leading 'http(s)://(www.) from URL'"""
         if url.startswith('http'):
             url = re.sub(r'https?://', '', url)
-            #print(url)
         if url.startswith('www.'):
             url = re.sub(r'www.', '', url)
         return url
@@ -339,7 +282,6 @@ def create_filtered_df(r_domains,l_domains):
                 try:
                     return parser.parse(x)
                 except ValueError:
-                    #print(x)
                     return None
             elif type(x) == datetime.datetime:
                 return x
@@ -356,27 +298,29 @@ def create_filtered_df(r_domains,l_domains):
     filtered_is_AP = []
 
     if args.do_serp:
-        google_cc_urls = pickle.load(open('google_search_res_climate_change.pkl','rb'))
+        google_pkl = glob.glob("google_search_res_climate_change*")
+        for g_pkl in google_pkl:
+            google_cc_urls = pickle.load(open(g_pkl,'rb'))
 
-        for key in google_cc_urls:
-            for keyword in google_cc_urls[key]:
-                for item in google_cc_urls[key][keyword]:
-                    url = strip_url(item[1])
-                    if not is_rss(url) and not is_blacklist(url) and not is_pdf(url):
-                        title = item[0]
-                        date = item[2] if len(item) > 2 else None
-                        stance = 'pro' if key in l_domains else 'anti'#get_google_res_stance(url)
-                        topic = 'cc'
-                        is_AP = None
+            for key in google_cc_urls:
+                for keyword in google_cc_urls[key]:
+                    for item in google_cc_urls[key][keyword]:
+                        url = strip_url(item[1])
+                        if not is_rss(url) and not is_blacklist(url) and not is_pdf(url):
+                            title = item[0]
+                            date = item[2] if len(item) > 2 else None
+                            stance = 'pro' if key in l_domains else 'anti'
+                            topic = 'cc'
+                            is_AP = None
 
-                        if ' | ' not in title:
-                            filtered_urls.append(url)
-                            filtered_titles.append(title)
-                            filtered_dates.append(date)
-                            filtered_domains.append(key)
-                            filtered_stances.append(stance)
-                            filtered_topics.append(topic)
-                            filtered_is_AP.append(is_AP)
+                            if ' | ' not in title:
+                                filtered_urls.append(url)
+                                filtered_titles.append(title)
+                                filtered_dates.append(date)
+                                filtered_domains.append(key)
+                                filtered_stances.append(stance)
+                                filtered_topics.append(topic)
+                                filtered_is_AP.append(is_AP)
 
     if args.do_mediacloud:
         mediacloud_cc_urls = pd.read_pickle('mediacloud_df.pkl')
@@ -408,53 +352,86 @@ def create_filtered_df(r_domains,l_domains):
                               'stance':filtered_stances,
                               'topic':filtered_topics,
                               'is_AP':filtered_is_AP})
+
     combined_df['domain'] = combined_df.domain.apply(standardize_domain)
     combined_df.title = combined_df.title.apply(lambda x: x.strip().lower() if x
                                            is not None else x)
     stance_reg_dict = {'l':'pro','r':'anti','c':'between','pro':'pro','anti':'anti','between':'between'}
     combined_df = combined_df.loc[~pd.isnull(combined_df.stance)]
     combined_df['stance'] = combined_df.stance.apply(lambda x: stance_reg_dict[x])
-    #combined_df.stance.value_counts()
     combined_df['date'] = combined_df['date'].apply(standardize_date)
+
     # We sort combined_df by title and date so that when we drop duplicate URLs,
     # we keep the one that doesn't have a null value for these fields.
     combined_df = combined_df.sort_values(['title'],axis=0)
-    combined_df = combined_df.drop_duplicates(subset='url',keep='first')#,ignore_index=True)
+    combined_df = combined_df.drop_duplicates(subset='url',keep='first')
     print('Intermediate df shape:',combined_df.shape)
     print('Saving intermediate df to "temp_combined_df.pkl"...')
     combined_df.to_pickle('temp_combined_df.pkl')
 
+
 if __name__ == "__main__":
+
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('--google_domains', type=str, default=None,
-                      help='/path/to/domains')
+                      help='/path/to/txt/file/with/domains')
     arg_parser.add_argument('--do_serp', action="store_true", help='whether to run SerpAPI URL retrieval')
     arg_parser.add_argument('--do_mediacloud', action="store_true", help='whether to run MediaCloud URL retrieval')
-
+    arg_parser.add_argument('--mediacloud_start_year', type=int, default=2000, help='Start year threshold for stories')
+    arg_parser.add_argument('--mediacloud_start_month', type=int, default=1, help='Start month threshold for stories')
+    arg_parser.add_argument('--mediacloud_start_day', type=int, default=1, help='Start day threshold for stories')
+    arg_parser.add_argument('--mediacloud_end_year', type=int, default=2020, help='End year threshold for stories')
+    arg_parser.add_argument('--mediacloud_end_month', type=int, default=12, help='End month threshold for stories')
+    arg_parser.add_argument('--mediacloud_end_day', type=int, default=31, help='End day threshold for stories')
     args = arg_parser.parse_args()
 
-    # Read in list of domains and political leanings for SerpAPI
-    if args.google_domains is None:
-        L_DOMAINS,R_DOMAINS = [],[]
-        with open('google_domains.txt','r') as f:
-            lines = f.readlines()
-            for line in lines:
-                split_line = line.split()
-                R_DOMAINS.append(split_line[0]) if split_line[1] == 'R' else L_DOMAINS.append(split_line[0])
-    else:
-        domain_dict = pickle.load(open(args.google_domains,'rb'))
-        L_DOMAINS = domain_list['L']
-        R_DOMAINS = domain_list['R']
 
     print('Getting URLs...')
     if args.do_serp:
-        if args.google_domains is not None:
-            get_serp_urls(domain_dict)
-        else:
-            get_serp_urls()
+        # Set up SerpAPI
+        from serpapi.google_search_results import GoogleSearchResults
+
+        if os.path.exists('./SERP_API_KEY.txt'):
+            with open('SERP_API_KEY.txt','r') as f:
+                SERP_API_KEY = f.read().strip()
+
+        query_params = {"location":"United States", "device":"desktop", "hl":"en", "gl":"us", "serp_api_key":SERP_API_KEY}
+        CC_KEYWORDS = ['climate_change','global_warming','fossil_fuels','carbon_dioxide','co2']
+        client = GoogleSearchResults(query_params)
+
+        # Read in list of domains and political leanings for SerpAPI
+        print("Reading in political leanings of domains for SerpAPI...")
+        fname = "google_domains.txt" if args.google_domains is None else args.google_domains
+
+        L_DOMAINS,R_DOMAINS = [],[]
+        with open(fname,'r') as f:
+            lines = f.read().splitlines()
+            for line in lines[1:]:
+                split_line = line.split('\t')
+                R_DOMAINS.append(split_line[0]) if split_line[1] == 'R' else L_DOMAINS.append(split_line[0])
+
+        print('L_DOMAINS:',L_DOMAINS)
+        print('R_DOMAINS:',R_DOMAINS)
+
+        #get_serp_urls(L_DOMAINS,R_DOMAINS)
+
     if args.do_mediacloud:
-        get_mc_urls()
+
+        # Set up MediaCloud API
+        import mediacloud.api
+
+        if os.path.exists('./MC_API_KEY.txt'):
+            with open('MC_API_KEY.txt','r') as f:
+                MC_API_KEY = f.read().strip()
+
+        mc = mediacloud.api.MediaCloud(MC_API_KEY)
+        mc_metadata = ['ap_syndicated','language','media_id','media_name','publish_date','title','guid','url','word_count']
+        mc_ids = pd.read_csv('./mediacloud_ids.txt',sep='\t',header=0)
+        mc_ids.reset_index(drop=True, inplace=True)
+
+        get_mc_urls(args.mediacloud_start_year,args.mediacloud_start_month,args.mediacloud_start_day,
+        args.mediacloud_end_year,args.mediacloud_end_month,args.mediacloud_end_day)
     print('Done retrieving URLs!')
 
     print('Creating intermediate dataframe...')
-    create_filtered_df(set(R_DOMAINS),set(L_DOMAINS))
+    create_filtered_df(set(L_DOMAINS),set(R_DOMAINS)) if L_DOMAINS is not None and R_DOMAINS is not None else create_filtered_df()
--- 1_data_scraping/README.md
+++ 1_data_scraping/README.md
@@ -8,5 +8,9 @@ Out of copyright concerns, we do not make the full text of the corresponding URL
 
 	To run `0_get_urls.py`, you need to get API keys for both. You can register for a MediaCloud API key for free; SerpAPI is only free within a limited trial period. Once you have gotten API keys, either copy them to the respective fields in `config.json` (in the root directory), or save them to files named 'MC_API_KEY.txt' and 'SERP_API_KEY.txt', respectively, in your local copy of this sub-directory.
 
+	You can use the default news domains that we used to retrieve news stories (`google_domains.txt` contains domains we fetched from using SerpApi, `mediacloud_ids.txt` contains domains we fetched from with the MediaCloud API), or you can modify these text files to add/remove domains as you like. Note, however, that MediaCloud requires an ID corresponding to a news domain to fetch from it (see their instructions [here](https://github.com/berkmancenter/mediacloud/blob/master/doc/api_2_0_spec/api_2_0_spec.md#grab-all-stories-in-the-new-york-times-during-october-2012)).
+
+Sample usage:
+
 
 * `1_dedup_titles.py` regularizes the headlines of fetched URLs for deduplication purposes.
