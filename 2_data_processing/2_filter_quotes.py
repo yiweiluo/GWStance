cecf04cd4e3a6dc3aebe32e167b7a748893f4697
Yiwei Luo <yiweiluo@Yiweis-new-Air.fios-router.home>
2020-07-23 14:07:53 -0400

    add householder filter, debug option

--- 2_data_processing/2_filter_quotes.py
+++ 2_data_processing/2_filter_quotes.py
@@ -19,11 +19,13 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 import spacy
 from spacy.lemmatizer import Lemmatizer, ADJ, NOUN, VERB
 
-# os.chdir('../')
-# print(os.getcwd())
-# print(os.listdir(os.getcwd()))
-# from local_processors import mv_files
-# os.chdir('./2_data_processing')
+nlp = spacy.load('en')
+lemmatizer = nlp.vocab.morphology.lemmatizer
+ps = PorterStemmer()
+
+config = json.load(open('../config.json', 'r'))
+BASE_DIR = config["BASE_DIR"]
+
 
 def mv_files(subdir_name,outerdir_name):
     """Moves contents of subdir_name (usually smaller batches) to outerdir_name."""
@@ -36,24 +38,15 @@ def mv_files(subdir_name,outerdir_name):
     print('New size of outerdir:',len(os.listdir(outerdir_name)))
     shutil.rmtree(os.path.join(outerdir_name,subdir_name))
 
-nlp = spacy.load('en')
-lemmatizer = nlp.vocab.morphology.lemmatizer
-ps = PorterStemmer()
-
-config = json.load(open('../config.json', 'r'))
-BASE_DIR = config["BASE_DIR"]
-QUOTES_DIR = os.path.join(BASE_DIR,'2_data_processing','url_quotes')
-
-householder_verbs = pd.read_pickle(open('householder_verbs.pkl','rb'))
-with open('pronouns.txt','r') as f:
-    PRONOUNS = set(f.read().splitlines())
 
 def read_stem_str(s):
     """Reads string formatted set of stems into an iterable list."""
     return s[2:-2].split("', '")
 
+
 def stem(s):
     return [ps.stem(w) for w in word_tokenize(s.lower())]
+
 
 def read_quote_json(url_guid,quotes_dir):
     """Reads json file containing quote objects associated with url_guid."""
@@ -63,37 +56,46 @@ def read_quote_json(url_guid,quotes_dir):
             return json.loads(contents)
         return None
 
-def get_householder_main_v_quotes(quote_tag_dict_list):
+
+def contains_keyword(stem_set,filter_dict):
+    """Returns True if stem_set contains a climate change-related keyword stem"""
+    return len(set(stem_set).intersection(filter_dict['keyword_stems'])) > 0
+
+
+def get_householder_main_v_quotes(quote_tag_dict_list,debug=False):
     """Given a labeled sentence, returns the Quotes in the sentence with a Householder verb main verb."""
-    good_quotes = [(q_no,q_dict) for q_no,q_dict in enumerate(quote_tag_dict_list['quotes'])]
-    #for q_no,q_dict in enumerate(quote_tag_dict_list['quotes']):
-        #main_v_indices = q_dict['main_v']#[x for x in q_dict if q_dict[x] == 'main_v']
-        #main_v_lemmas = [quote_tag_dict_list['idx2lemma'][x] for x in sorted(main_v_indices,key=lambda x:int(x))]
-        #main_v_lemmas = [lemmatizer(tok.lower(),VERB)[0] for tok in main_v_toks]
-        #main_v_lemmas.append('_'.join(main_v_lemmas))
-        #main_v_lemmas = set(main_v_lemmas)
-        #if len(main_v_lemmas.intersection(householder_verbs)) > 0:
-        #good_quotes.append((q_no,q_dict))
+    good_quotes = []
+    for q_no,q_dict in enumerate(quote_tag_dict_list['quotes']):
+        main_v_indices = q_dict['main_v']#[q_dict[x] for x in q_dict if x == 'main_v']
+        #print(main_v_indices)
+        #main_v_toks = [quote_tag_dict_list['idx2text'][x] for x in sorted(main_v_indices)]
+        main_v_lemmas = [quote_tag_dict_list['idx2lemma'][str(x)] for x in sorted(main_v_indices)]
+        v_prt_indices = q_dict['v_prt']#[x for x in q_dict if q_dict[x] == 'v_prt']
+        main_v_prts = [quote_tag_dict_list['idx2lemma'][str(x)] for x in sorted(v_prt_indices)]
+        full_main_vs = main_v_lemmas.copy()
+        for l in main_v_lemmas:
+            full_main_vs.extend(['_'.join([l,v_prt]) for v_prt in main_v_prts])
+        full_main_vs = set(full_main_vs)
+        #main_v_lemmas_small = [x for x in main_v_lemmas if x not in AUX]
+        if debug:
+            print('quote_dict':q_dict)
+            print('\tMain verb lemmas:',main_v_lemmas)
+            print('\tFull main verb lemmas:',full_main_vs)
+        householder_intersec = full_main_vs.intersection(householder_verbs)
+        if len(householder_intersec) > 0:
+            if debug:
+                print('\t\tIn Householder set!')
+                print('\t\t'+householder_intersec)
+            good_quotes.append((q_no,q_dict))
 
     return good_quotes
 
-with open('filtering_keywords.txt','r') as f:
-    lines = f.read().splitlines()
-    KEYWORDS = set([l.split('\t')[0] for l in lines])
-    KEYWORD_STEMS = set([l.split('\t')[1] for l in lines])
-
-def contains_keyword(stem_set):
-    """Returns True if stem_set contains a CC keyword stem"""
-    return len(set(stem_set).intersection(KEYWORD_STEMS)) > 0
 
 
-def main(df_path,output_dir,quotes_dir):
+def main(output_dir,quotes_dir,filter_dict,stop_ix):
     """Writes complement clauses of quotes (filtered down to those with a Householder stem as the quoting verb) to 'all_quote_comps.csv'.
     Then, finds stems of all tokens in filtered complement clauses. Finally, removes indirect questions and filters comp. clauses again
     by keyword stems and writes to `keyword_filtered_comp_clauses.tsv`."""
-
-    # Load data
-    df = pd.read_csv(df_path,sep='\t',header=0)
 
     # Set-up CSV with quote comp clauses
     fieldnames = ['guid', 'sent_no', 'quote_no', 'quote_text', 'coref']
@@ -101,7 +103,7 @@ def main(df_path,output_dir,quotes_dir):
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
-    for n,url_guid in enumerate(df.guid):
+    for n,url_guid in enumerate(df.guid[:stop_ix]):
 
         # Read in parsed results
         obj = read_quote_json(url_guid,quotes_dir)
@@ -116,10 +118,10 @@ def main(df_path,output_dir,quotes_dir):
             good_v_quotes = []
             for sent_no in sents_with_quotes:
                 # Get quotes with main verb that is one of interest (in Householder list)
-                #householder_main_v_quotes = get_householder_main_v_quotes(obj['quote_tags'][sent_no])
+                householder_main_v_quotes = get_householder_main_v_quotes(obj['quote_tags'][sent_no])
                 quote_tag_dict_list = obj['quote_tags'][sent_no]
-                #good_v_quotes.extend(list(zip([sent_no]*len(householder_main_v_quotes),householder_main_v_quotes)))
-                good_v_quotes.extend([(sent_no,q_no,q_dict) for q_no,q_dict in enumerate(quote_tag_dict_list['quotes'])])
+                good_v_quotes.extend(list(zip([sent_no]*len(householder_main_v_quotes),householder_main_v_quotes)))
+                #good_v_quotes.extend([(sent_no,q_no,q_dict) for q_no,q_dict in enumerate(quote_tag_dict_list['quotes'])])
 
             # Now, get the quote text to classify stance, with url_guid + sent_no, so I can recover context
             good_v_quote_texts = [(sent_no,q_no,[(obj['quote_tags'][sent_no]['idx2text'][str(idx)],obj['coref_tags'][str(idx)])
@@ -130,7 +132,7 @@ def main(df_path,output_dir,quotes_dir):
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                 for sent_no,q_no,q_text in good_v_quote_texts:
                     writer.writerow({'guid': url_guid, 'sent_no': sent_no, 'quote_no': q_no,
-                                     'coref': ' '.join([x[1] if x[0].lower() in PRONOUNS and x[1] is not None
+                                     'coref': ' '.join([x[1] if x[0].lower() in filter_dict['pronouns'] and x[1] is not None
                                                            else x[0] for x in q_text]),
                                     'quote_text': ' '.join([x[0] for x in q_text])})
 
@@ -163,14 +165,25 @@ def main(df_path,output_dir,quotes_dir):
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--debug', action="store_true", default=None,
+                      help='whether to test run on smaller sample first')
     arg_parser.add_argument('--path_to_df', type=str, default=None,
-                      help='/path/to/df')
+                      help='where to read in df of articles')
     arg_parser.add_argument('--output_dir', type=str, default=None,
-                      help='where to write batched output')
+                      help='where to write output')
     arg_parser.add_argument('--quotes_dir', type=str, default=None,
-                      help='where to source fulltext')
+                      help='where to source extracted quotes')
 
     args = arg_parser.parse_args()
+
+    # Load data
+    df = pd.read_csv(args.path_to_df,sep='\t',header=0)
+
+    if args.debug:
+        end_ix = 2
+        print("Debug mode ON. Sample output for the first 5 articles will be written to .txt files prefixed with 'sample_output_'.")
+    else:
+        end_ix = len(df)
 
     # Move files from inside batches
     print('Moving the following quote extraction batches to {}:\n'.format(args.quotes_dir),
@@ -178,4 +191,18 @@ if __name__ == "__main__":
     for subdir_path in glob.glob(os.path.join(args.quotes_dir,'extracted_quotes_*')):
         mv_files(subdir_path.split('/')[-1],args.quotes_dir)
 
-    main(args.path_to_df,args.output_dir,args.quotes_dir)
+
+    print("Reading in Householder verbs, pronouns, and keywords for filtering...")
+    householder_verbs = pd.read_pickle(open('householder_verbs.pkl','rb'))
+    with open('pronouns.txt','r') as f:
+        PRONOUNS = set(f.read().splitlines())
+    with open('filtering_keywords.txt','r') as f:
+        lines = f.read().splitlines()
+        KEYWORDS = set([l.split('\t')[0] for l in lines])
+        KEYWORD_STEMS = set([l.split('\t')[1] for l in lines])
+    FILTER_WORDS = {'pronouns':PRONOUNS,'householder':householder_verbs,
+    'keywords':KEYWORDS,'keyword_stems':KEYWORD_STEMS}
+    for filter_type in FILTER_WORDS:
+        print('\tNumber of {}:'.format(filter_type),len(FILTER_WORDS[filter_type]))
+
+    main(args.output_dir,args.quotes_dir,FILTER_WORDS,end_ix)
