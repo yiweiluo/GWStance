514aec0b461961be3b029af1b815b820c277ba90
Yiwei Luo <yiweiluo@Yiweis-new-Air.fios-router.home>
2020-07-24 10:53:22 -0400

    comment out unnecessary lines, add command line args, remove redundant save_df

--- 2_data_processing/3_prep_quotes_for_classif.py
+++ 2_data_processing/3_prep_quotes_for_classif.py
@@ -4,10 +4,13 @@ import os
 import pandas as pd
 import numpy as np
 import re
+import argparse
 
-def read_stem_str(s):
-    """Reads string formatted set of stems into an iterable list."""
-    return s[2:-2].split("', '")
+
+# def read_stem_str(s):
+#     """Reads string formatted set of stems into an iterable list."""
+#     return s[2:-2].split("', '")
+
 
 def prettify(clause):
     """Clean comp. clause for classification."""
@@ -25,27 +28,34 @@ def prettify(clause):
 
     return clause_str
 
+
 if __name__ == "__main__":
-    quotes_df = pd.read_csv('./output/keyword_filtered_comp_clauses.tsv',sep='\t',header=0,index_col=0)
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--path_to_input', type=str, default=os.path.join('output','keyword_filtered_comp_clauses.tsv'),
+                      help='where to read in the filtered output from `2_filter_quotes.py`')
+    arg_parser.add_argument('--output_dir', type=str, default=None,
+                      help='where to write output')
+
+    args = arg_parser.parse_args()
+
+    quotes_df = pd.read_csv(args.path_to_input,sep='\t',header=0,index_col=0)
     # Parse string list into actual list of stems
     #quotes_df['quote_stem_list'] = quotes_df['quote_stems'].apply(read_stem_str)
     #quotes_df['quote_stem_list_coref'] = quotes_df['quote_stems_coref'].apply(read_stem_str)
 
-    print('Cleaning comp. clauses for classification...')
+    print('Transforming comp. clauses for classification...')
     quotes_df['clean_quote'] = quotes_df['quote_text'].apply(prettify)
     quotes_df['clean_quote_coref'] = quotes_df['coref'].apply(prettify)
-    print('Saving...')
 
+    print('Saving...')
     save_df = quotes_df[['guid','sent_no','quote_no','clean_quote','clean_quote_coref']]
-    save_df.to_csv('./output/keyword_filtered_comp_clauses_for_classif.tsv'
-                                                                            ,sep='\t',header=True)
+    # save_df.to_csv('./output/keyword_filtered_comp_clauses_for_classif.tsv'
+    #                                                                          ,sep='\t',header=True)
     batch_size = 50000
-    os.mkdir('./output/batched')
+    os.mkdir(os.path.join(args.output_dir,'batched'))
     for batch_no in range(round(len(save_df)/batch_size+0.5)):
-        os.mkdir('./output/batched/{}'.format(batch_no))
+        os.mkdir(os.path.join(args.output_dir,'batched',str(batch_no)))
         batch_df = save_df[batch_no*batch_size:batch_no*batch_size+batch_size]
-        batch_df.to_csv('./output/batched/{}/test.tsv'.format(batch_no),sep='\t',header=True)
+        batch_df.to_csv(os.path.join(args.output_dir,'batched',str(batch_no),'test.tsv'),sep='\t',header=True)
 
     print('Done!\n')
-
-    # Want to divide into batches for prediction?
