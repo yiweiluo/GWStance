3837f92eb4e39652fadff601be63606cb9b81200
Yiwei Luo <yiweiluo@Yiweis-new-Air.fios-router.home>
2020-10-15 12:31:39 -0400

    fig saving bug

--- 1_data_scraping/3_explore_dataset.py
+++ 1_data_scraping/3_explore_dataset.py
@@ -376,7 +376,8 @@ def create_data_report(dated_df):
                              for d in dated_df.date]
     dated_df['month'] = [d.to_pydatetime().month
                              for d in dated_df.date]
-    fig = dated_df.loc[dated_df.year.isin(range(2000,2021))].year.plot.hist() 
+    fig = dated_df.loc[dated_df.year.isin(range(2000,2021))].year.plot.hist()
+    plt.show()
     plt.savefig('output/temporal_distribution.png')
 
     print('\nDistribution of article outlets over time:')
